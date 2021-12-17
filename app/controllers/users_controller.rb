2fe60f33581f784a6cd1821bea1e9457ef750b53
ahartfelder <ahartfelder@gmail.com>
2021-12-17 17:24:16 -0300

    Added Users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
   def new
     @user = User.new
+  end
+  def edit
   end
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to My Blog '#{@user.username}'! You have successfully signed up."
+      flash[:notice] = "Welcome to My Blog '#{@user.username}'! You have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
     end      
   end
-
+  def update
+    if User.update(user_params)
+      flash[:notice] = "Your account has been successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   private
-
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
