8d2f44c91cd3b95138ba9a860b097e5bfad19ba5
ahartfelder <ahartfelder@gmail.com>
2021-12-17 10:41:35 -0300

    Added User-Article Association plus some styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { in: 5..50 }
   validates :description, presence: true, length: { in: 10..300 }
 end
