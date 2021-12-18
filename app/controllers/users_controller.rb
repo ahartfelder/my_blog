57e183ee56442cf936020a2ec84610288476a6b6
ahartfelder <ahartfelder@gmail.com>
2021-12-18 09:09:36 -0300

    added user-sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update]
+  before_action :set_user, only: %i[show edit update destroy]
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+  end
+  
   def new
     @user = User.new
   end
+  
   def edit
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,19 +24,30 @@ class UsersController < ApplicationController
       render 'new'
     end      
   end
+  
   def update
     if User.update(user_params)
       flash[:notice] = "Your account has been successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  def destroy
+    @username = @user.username
+    @user.destroy
+    flash[:notice] = "#{@username.capitalize} has been permanently removed"
+  end
+  
   private
+  
   def set_user
     @user = User.find(params[:id])
   end
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { in: 3..25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true, length: { in: 6..15 }
   has_secure_password
 end
