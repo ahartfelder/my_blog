d12cc6f7cc828278919bbb9ac7432c677ee195d2
ahartfelder <ahartfelder@gmail.com>
2021-12-19 16:52:17 -0300

    authentication and protection done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
   
   def index
     @users = User.all
@@ -18,7 +20,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to My Blog '#{@user.username}'! You have successfully signed up"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to My Blog #{@user.username}! You have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
@@ -35,9 +38,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @username = @user.username
     @user.destroy
-    flash[:notice] = "#{@username.capitalize} has been permanently removed"
+    session[:user_id] = nil if @user = current_user
+    flash[:notice] = "Account and associated articles have been permanently removed"
+    redirect_to articles_path
   end
   
   private
@@ -48,6 +52,13 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only modify or delete your own profile"
+      redirect_to @user
+    end
   end
 
 end
